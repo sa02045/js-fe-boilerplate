5a90e4785768d7e9192013720d1def6a09dcf9dd
DevJo <sa02045@naver.com>
2022-03-21 23:08:11 +0900

    chore: wepback config 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,34 @@
 const path = require("path");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = {
-  entry: "./src/main.js",
+  entry: "./src/js/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  devtool: "source-map",
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "keyoboard",
+      template: "./index.html",
+      inject: "body",
+    }),
+    new MiniCssExtractPlugin({ filename: "style.css" }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserWebpackPlugin(), new CssMinimizerPlugin()],
   },
 };
